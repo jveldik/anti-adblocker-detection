4aeeea69ae5a685db7a796728600df35b7ea530f
jveldik <eldik.jorn@gmail.com>
2023-06-06 00:51:02 +0200

    first working version

--- data_generation.py
+++ data_generation.py
@@ -1,43 +1,54 @@
 import csv
 import pickle
 import os.path
+import tkinter
 from tkinter import messagebox
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 # Load the firefox profile with extensions
-profile = webdriver.FirefoxProfile("5jqn9t8w.blockerProfile")
+options = Options()
+options.add_argument("-profile")
+options.add_argument("5jqn9t8w.blockerProfile")
+# options.set_preference('profile', "5jqn9t8w.blockerProfile")
 # Set up the Selenium firefox browser
-driver = webdriver.Firefox(profile)
+driver = webdriver.Firefox(options=options)
 driver.maximize_window()
 
-# Make list to save all results
-if os.path.exists("data/top5000.pickle"):
-    with open("data/top5000.pickle", "rb") as f:
-        top5000 = pickle.load(f)
+# Determine how many is already done
+start = 0
+if os.path.exists("data/top5000.csv"):
+    with open("data/top5000.csv", "r") as f:
+        start = sum(1 for line in f)-1
 else:
-    top5000 = []
+    with open("data/top5000.csv", "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["URL", "usesAntiAdBlocker"])
 
 # Load the top 5000 urls that have not been loaded yet
 with open("data/top-1m.csv", "r") as f:
     reader = csv.reader(f)
-    urls = [row[1] for i, row in enumerate(reader) if i >= len(top5000) and i < 5000]
+    urls = [row[1] for i, row in enumerate(reader) if i >= start and i < 5000]
 
 # Loop over the URLs
 for url in urls:           
     # Use Selenium to load the URL and capture the HTTP requests and responses
-    driver.get(url)
+    driver.get(f"http://{url}")
+    root = tkinter.Tk()
+    root.wm_attributes("-topmost", 1)
+    root.withdraw()
     answer = messagebox.askyesnocancel(title="Label site", message="Does this site use an anti adblocker?", default="no")
     if answer == None:
-        # Save the dictionary
-        with open("data/top5000.pickle", 'wb') as f:
-            pickle.dump(top5000, f)
         # Quit the selenium instance
         driver.quit()
         break
     # Save HTML page source
     pageSource = driver.page_source
-    with open(f"data/page_sources/{url}.html", 'w') as f:
+    with open(f"data/page_sources/{url}.html", 'w', encoding="utf-8") as f:
         f.write(pageSource) 
     # Save screenshot
     driver.save_screenshot(f"data/screenshots/{url}.png")
-    top5000.append(url, answer)
+    # Save label
+    with open('data/top5000.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([url, answer])
