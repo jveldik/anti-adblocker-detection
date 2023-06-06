db8193094127b89f3705a333459cdaa3e09ebd63
jveldik <eldik.jorn@gmail.com>
2023-06-06 14:30:19 +0200

    Added try except

--- data_generation.py
+++ data_generation.py
@@ -1,5 +1,4 @@
 import csv
-import pickle
 import os.path
 import tkinter
 from tkinter import messagebox
@@ -31,24 +30,30 @@ with open("data/top-1m.csv", "r") as f:
     urls = [row[1] for i, row in enumerate(reader) if i >= start and i < 5000]
 
 # Loop over the URLs
-for url in urls:           
-    # Use Selenium to load the URL and capture the HTTP requests and responses
-    driver.get(f"http://{url}")
-    root = tkinter.Tk()
-    root.wm_attributes("-topmost", 1)
-    root.withdraw()
-    answer = messagebox.askyesnocancel(title="Label site", message="Does this site use an anti adblocker?", default="no")
-    if answer == None:
-        # Quit the selenium instance
-        driver.quit()
-        break
-    # Save HTML page source
-    pageSource = driver.page_source
-    with open(f"data/page_sources/{url}.html", 'w', encoding="utf-8") as f:
-        f.write(pageSource) 
-    # Save screenshot
-    driver.save_screenshot(f"data/screenshots/{url}.png")
-    # Save label
-    with open('data/top5000.csv', 'a', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow([url, answer])
+for url in urls:    
+    try:       
+        # Use Selenium to load the URL and capture the HTTP requests and responses
+        driver.get(f"http://{url}")
+        root = tkinter.Tk()
+        root.wm_attributes("-topmost", 1)
+        root.withdraw()
+        answer = messagebox.askyesnocancel(title="Label site", message="Does this site use an anti adblocker?", default="no")
+        if answer == None:
+            # Quit the selenium instance
+            driver.quit()
+            break
+        # Save HTML page source
+        pageSource = driver.page_source
+        with open(f"data/page_sources/{url}.html", 'w', encoding="utf-8") as f:
+            f.write(pageSource) 
+        # Save screenshot
+        driver.save_screenshot(f"data/screenshots/{url}.png")
+        # Save label
+        with open('data/top5000.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([url, answer])
+    except:
+        print(f"There was an exception with {url}")
+        with open('data/top5000.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([url, None])
