3b3dcf459cccbd924c4dcea47139b0f5dc0c7b7e
jveldik <eldik.jorn@gmail.com>
2023-08-18 23:42:20 +0200

    changed scripts to array

--- feature_extraction.py
+++ feature_extraction.py
@@ -23,8 +23,6 @@ class FeatureExtractor(ASTVisitor):
 
 # Function to fetch and process external JavaScript content
 def fetch_external_js(url, source):
-    print(url)
-    print(source)
     if source.startswith('/'):
         url = f"{url}{source}"
     else:
@@ -37,7 +35,7 @@ def fetch_external_js(url, source):
 
 # Function to extract JavaScript code from HTML file
 def extract_scripts(html_path):
-    scripts = ""
+    scripts = []
     with open(html_path, 'r', encoding='utf-8') as f:
         html_code = BeautifulSoup(f.read(), 'html.parser')
         url = "http://" + html_path.replace('data/page_sources/', '').replace('.html', '')
@@ -48,30 +46,26 @@ def extract_scripts(html_path):
             if source:
                 script_content = fetch_external_js(url, source)
                 if script_content:
-                    scripts += script_content + "\n"
+                    scripts.append(script_content)
             else:
-                scripts += f"{source_tag.string}\n"
-    print(scripts)
+                scripts.append(source_tag.string)
     return scripts
 
 # Function to extract features from JavaScript code
-def extract_features(scripts):
-    parser = Parser()
-    tree = parser.parse(scripts)
-    
-    extractor = FeatureExtractor()
-    extractor.visit(tree)
-    
-    return extractor.feature_sets
+def extract_features(parser, extractor, scripts):
+    for script in scripts:
+        tree = parser.parse(script)
+        extractor.visit(tree)
+        break
 
 # Iterate through HTML files and extract features
 page_sources_path = "data/page_sources/"
+parser = Parser()
+extractor = FeatureExtractor()
 for file in os.listdir(page_sources_path):
     if file.endswith('.html'):
         html_path = os.path.join(page_sources_path, file)
         scripts = extract_scripts(html_path)
-        feature_sets = extract_features(scripts)
-        print(f"Features extracted from {file}:")
-        for name, features in feature_sets.items():
-            print(name, features)
+        feature_sets = extract_features(parser, extractor, scripts)
         break
+print(extractor.feature_sets)
