c60b2cfa8a73f0de4c527fee6494de7f748b99b1
jveldik <eldik.jorn@gmail.com>
2023-10-08 21:10:26 +0200

    Working code for feature set extraction

--- feature_sets_extraction.py
+++ feature_sets_extraction.py
@@ -1,116 +1,115 @@
+import csv
 import os
 import pickle
 import esprima
-
-scripts = ["""if (new Date().getHours() < 18) {
-  document.getElementById("demo").innerHTML = "Good day!";
-}"""
-#     ,"""
-# function BlockAdBlock(options) {
-#     this._options = options || {};
-#     this._var = {};
-#     this.init();
-# }
-
-# BlockAdBlock.prototype.init = function() {
-#     this._var.bait = null;
-#     this._creatBait();
-#     this._checkBait(true);
-# };
-
-# BlockAdBlock.prototype._creatBait = function() {
-#     var bait = document.createElement('div');
-#     bait.setAttribute('class', this._options.baitClass);
-#     bait.setAttribute('style', this._options.baitStyle);
-#     this._var.bait = window.document.body.appendChild(bait);
-#     this._var.bait.offsetParent;
-#     this._var.bait.offsetHeight;
-#     this._var.bait.offsetLeft;
-#     this._var.bait.offsetTop;
-#     this._var.bait.offsetWidth;
-#     this._var.bait.clientHeight;
-#     this._var.bait.clientWidth;
-#     if (this._options.debug === true) {
-#         this._log('_creatBait', 'Bait has been created');
-#     }
-# };
-
-# BlockAdBlock.prototype._checkBait = function(loop) {
-#     var detected = false;
-#     if (window.document.body.getAttribute('abp') !== null ||
-#         this._var.bait.offsetParent === null ||
-#         this._var.bait.offsetHeight === 0 ||
-#         this._var.bait.offsetLeft === 0 ||
-#         this._var.bait.offsetTop === 0 ||
-#         this._var.bait.offsetWidth === 0 ||
-#         this._var.bait.clientHeight === 0 ||
-#         this._var.bait.clientWidth === 0) {
-#         detected = true;
-#     }
-# };
-# """
-]
+from scipy.sparse import lil_matrix
+from sklearn.feature_selection import chi2, SelectKBest, VarianceThreshold
 
 # Function to recursively extract features from an AST
-def extract_features(node, context, feature_sets):
+def extract_features_from_AST(node, new_features):
     if isinstance(node, esprima.nodes.Node):
-        node_type = node.type
-        current_context = context + [node_type]
-        if not node.name == None:
-            text = node.name
-        elif not node.value == None:
-            text = node.value
-        else:
-            text = ''
-        if text:
-            feature_sets[0].append(('.'.join(current_context), text))
-            if node_type == 'Literal':
-                feature_sets[1].append(('.'.join(current_context), text))
-            else:
-                feature_sets[2].append(('.'.join(current_context), text))
-
+        if node.type == 'Literal':
+            text = str(node.value)
+            if text and len(text) < 50:
+                new_features[0].append(text)
+                new_features[1].append(text)
+        elif node.type == 'Identifier':
+            text = str(node.name)
+            if text and len(text) < 50:
+                new_features[0].append(text)
+                new_features[2].append(text)
         for key, value in node.items():
             if isinstance(value, (list, esprima.nodes.Node)):
-                extract_features(value, current_context, feature_sets)
+                extract_features_from_AST(value, new_features)
     elif isinstance(node, list):
         for item in node:
-            extract_features(item, context, feature_sets)
+            extract_features_from_AST(item, new_features)
 
-# Parse JavaScript code and extract features
-def extract_features_from_js(script, feature_sets):
-    try:
-        ast = esprima.parseScript(script)
-        extract_features(getattr(ast, 'body'), [], feature_sets)
-    except Exception as e:
-        print(f"Error parsing JavaScript code: {e}")
+def extract_features_from_url(url, feature_lists):
+    new_features = [[],[],[]]
+    for dir in os.listdir(f"data/scripts/{url}"):
+        with open(f"data/scripts/{url}/{dir}", 'r', encoding='utf-8') as f:
+            script = f.read()
+        try:
+            ast = esprima.parseScript(script)
+        except Exception as e:
+            with open('parsing_errors.log', 'a') as error_log:
+                error_log.write(f"Error parsing JavaScript code with {url}/{dir}: {e}\n")
+            continue
+        extract_features_from_AST(getattr(ast, 'body'), new_features)
+    feature_lists[0].append(new_features[0])
+    feature_lists[1].append(new_features[1])
+    feature_lists[2].append(new_features[2])
+    print(f"Extracted features from {url}")
 
-def filter_features(features, number_of_features):
-    # Remove duplicates
-    features = list(set(features))
-    return features[:number_of_features]
+def extract_features():
+    feature_lists = [[], [], []]
+    labels = []
+    with open("data/labeled_urls.csv", mode = 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row and row[1]:
+                url = row[0]
+                labels.append(bool(row[1]))
+                extract_features_from_url(url, feature_lists)
+    return feature_lists, labels
 
-all_features = []
-literal_features = []
-identifier_features = []
-feature_sets = [all_features, literal_features, identifier_features]
-scripts_map_path = "data/scripts/"
-for dir in os.listdir(scripts_map_path):
-    url_path = os.path.join(scripts_map_path, dir)
-    if os.path.isdir(url_path):
-        for dir in os.listdir(url_path):
-            script_path = os.path.join(url_path, dir)
-            if dir.endswith('.js'):
-                with open(script_path, 'r', encoding='utf-8') as f:
-                    script = f.read()
-                    extract_features_from_js(script, feature_sets)
+def create_matrices(feature_lists):
+    feature_sets = [None, None, None]
+    matrices = [None, None, None]
+    # Loop over the three different kinds of features
+    for i, (matrix, feature_set, feature_list) in enumerate(zip(matrices, feature_sets, feature_lists)):
+        # Flatten list, remove duplicates, and sort the features
+        print("Creating feature set")
+        feature_set = sorted(list(set([feature for features in feature_list for feature in features])))
+        # Create a dictionary to map features to their indices
+        feature_index_map = {feature: index for index, feature in enumerate(feature_set)}
+        # Initialize sparse matrix
+        matrix = lil_matrix((len(feature_list), len(feature_set)), dtype=bool)
+        # Fill in the sparse matrix using the feature_index_map
+        print("Filling in the matrix")
+        for j, feature in enumerate(feature_list):
+            for feature_value in feature:
+                index = feature_index_map.get(feature_value)
+                if index is not None:
+                    matrix[j, index] = 1
+        print(matrix)
+        # Assign the computed feature_set and matrix to the respective lists
+        feature_sets[i] = feature_set
+        matrices[i] = matrix
+    return feature_sets, matrices
 
-set_names = ["all", "literal", "identifiers"]
-for (features, set_name)  in zip(feature_sets, set_names):
-    for number_of_features in [100, 1000, 10000]:
-        features = filter_features(features, number_of_features)
-        with open(f"data/feature_sets/{set_name}_{number_of_features}.pickle", 'wb') as f:
-            pickle.dump(features, f)
-    
-print(all_features)
-print(literal_features)
-print(identifier_features)
+def save_data(matrix, feature_set, i, k):
+    if i == 0:
+        set_name = "all"
+    elif i == 1:
+        set_name = "literal"
+    else:
+        set_name = "identifier"
+    with open(f"data/matrices/{set_name}_{k}.pickle", 'wb') as f:
+        pickle.dump(matrix, f)
+    with open(f"data/feature_sets/{set_name}_{k}.pickle", 'wb') as f:
+        pickle.dump(feature_set, f)
+
+def filter_matrices(matrices, feature_sets, labels):
+    for i, (matrix, feature_set) in enumerate(zip(matrices, feature_sets)):
+        # Initialize the VarianceThreshold
+        variance_threshold = VarianceThreshold(threshold=0.01)
+        # Fit and transform matrix to remove low variance features
+        print("Applying treshold")
+        matrix = variance_threshold.fit_transform(matrix)
+        feature_set = variance_threshold.get_feature_names_out(feature_set)
+        for k in [10000, 1000, 100]:
+            # Initialize SelectKBest with the chi-squared test scoring function and desired k value
+            selector = SelectKBest(score_func=chi2, k=k)
+            # Fit and transform matrix to select the top k features
+            print("Applying chi")
+            matrix = selector.fit_transform(matrix, labels)
+            feature_set = selector.get_feature_names_out(feature_set)
+            # Save the filtered matrix and feature set
+            print("Saving data")
+            save_data(matrix, feature_set, i, k)
+
+feature_lists, labels = extract_features()
+feature_sets, matrices = create_matrices(feature_lists)
+filter_matrices(matrices, feature_sets, labels)
