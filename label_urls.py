eda8455ebe9b53e4a935a3369f2e6062b7ec084b
jveldik <eldik.jorn@gmail.com>
2024-06-06 11:51:34 +0200

    Fixed starting at the right url

--- label_urls.py
+++ label_urls.py
@@ -12,16 +12,17 @@ def get_urls_to_label(filename):
             nr_labeled_urls = sum(1 for line in file)
     else:
         nr_labeled_urls = 0
-    # Read the stored URLs, starting after the last labeled URL
+    # Read the stored URLs
     with open(f"data/{filename}", mode='r', newline='') as file:
         reader = csv.reader(file)
-        urls = [(row[0], row[1]) for i, row in enumerate(reader) if i >= nr_labeled_urls]
+        urls = [(row[0], row[1]) for row in reader]
     # Split the URLs into true and false and interleave them
     true_urls = [url for url in urls if url[1] == 'True']
     false_urls = [url for url in urls if url[1] == 'False']
     min_len = min(len(true_urls), len(false_urls))
     interleaved_urls = [val for pair in zip(true_urls[:min_len], false_urls[:min_len]) for val in pair]
-    return interleaved_urls
+    # Return the interleaved urls, starting after the last labeled URL
+    return interleaved_urls[nr_labeled_urls:]
     
 def save_label(url, label):
     with open(f"data/manual_{filename}", "a", newline='') as file:
