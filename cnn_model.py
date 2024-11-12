34b9d1bdc94b15789d95fb2aaacb10df2825c35f
jveldik <eldik.jorn@gmail.com>
2024-11-12 20:30:44 +0100

    Hyperband with lr scheduler

--- cnn_model.py
+++ cnn_model.py
@@ -3,47 +3,50 @@ import pickle
 import numpy as np
 import pandas as pd
 from imblearn.over_sampling import SMOTE
-from keras_tuner import BayesianOptimization 
+from keras_tuner import Hyperband
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.utils.class_weight import compute_class_weight
-from tensorflow.keras.callbacks import EarlyStopping
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Conv1D, GlobalMaxPooling1D
+from tensorflow.keras.layers import Dense, Dropout, Conv1D, GlobalMaxPooling1D, BatchNormalization
 from tensorflow.keras.optimizers import Adam, RMSprop, SGD
 
 def create_cnn_model(hp, input_dim, class_weights):
     model = Sequential()
     num_layers = hp.Choice("num_layers", values=[1, 2, 3])
-    dense_dropout = hp.Choice('dense_dropout', values=[0.3, 0.5, 0.7])
+    dense_dropout = hp.Choice('dense_dropout', values=[0.2, 0.4, 0.6])
+    dense_units = hp.Choice(dense_units, values=[128, 256, 384, 512])
     optimizer_choice = hp.Choice("optimizer", values=["adam", "RMSprop", "SGD"])
-    learning_rate = hp.Choice("learning_rate", values=[1e-3, 1e-4, 1e-5])
+
     if optimizer_choice == "adam":
-        optimizer = Adam(learning_rate=learning_rate)
+        optimizer = Adam(learning_rate=0.001)
     elif optimizer_choice == "RMSprop":
-        optimizer = RMSprop(learning_rate=learning_rate)
+        optimizer = RMSprop(learning_rate=0.001)
     else:
-        optimizer = SGD(learning_rate=learning_rate)
+        optimizer = SGD(learning_rate=0.001)
 
     for i in range(num_layers):
         filters = hp.Choice(f'filters_{i}', values=[64, 128, 256])
         kernel_size = hp.Choice(f'kernel_size_{i}', values=[3, 5, 7])
-        dropout = hp.Choice(f'dropout_{i}', values=[0.3, 0.5, 0.7])
+        dropout = hp.Choice(f'dropout_{i}', values=[0.2, 0.4, 0.6])
         # Input shape only needed for the first layer
         if i == 0:
             model.add(Conv1D(filters=filters, kernel_size=kernel_size, activation='relu', input_shape=(input_dim, 1)))
         else:
             model.add(Conv1D(filters=filters, kernel_size=kernel_size, activation='relu'))
+        model.add(BatchNormalization())
         model.add(Dropout(dropout))
     
     model.add(GlobalMaxPooling1D())
-    model.add(Dense(units=hp.Int('dense_units', min_value=128, max_value=512, step=128), activation='relu'))
+    model.add(Dense(units=dense_units, activation='relu'))
     model.add(Dropout(dense_dropout))
     model.add(Dense(1, activation='sigmoid'))  # Binary classification output
-    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'], loss_weights=class_weights)
     return model
 
-def create_model(matrix, labels, number_of_features, balancing):
+def create_model(matrix, labels, set_name, number_of_features, balancing):
     # Reshape labels and matrix for CNN input
     labels = np.array(labels)
     matrix = matrix.toarray()
@@ -56,15 +59,14 @@ def create_model(matrix, labels, number_of_features, balancing):
 
     # Compute class weights based on the training labels
     class_weights = compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
-    # class_weights_dict = dict(enumerate(class_weights))
 
     # Initialize the tuner
-    tuner = BayesianOptimization(
+    tuner = Hyperband(
         lambda hp: create_cnn_model(hp, number_of_features, class_weights), 
         objective='val_accuracy',
         overwrite=True,
-        directory='.hidden_tuning',  # Hidden folder by prefixing with "."
-        project_name='temp_tuning'
+        directory='data/trials',
+        project_name=f"{set_name}_{number_of_features}_{balancing}"
     )
 
     # Run the hyperparameter search
@@ -73,7 +75,11 @@ def create_model(matrix, labels, number_of_features, balancing):
         y_train, 
         epochs=100, 
         validation_split=0.2, 
-        callbacks=[EarlyStopping(monitor='val_loss', patience=3)], 
+        callbacks=[
+            EarlyStopping(monitor='val_loss', patience=5),
+            ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, min_lr=1e-6)
+        ], 
+        workers=-1,
         verbose=0
     )
 
@@ -105,7 +111,8 @@ def create_model(matrix, labels, number_of_features, balancing):
 
 if __name__ == "__main__":
     # Remove unnecesary TensorFlow warnings
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    tf.get_logger().setLevel('ERROR')
 
     df = pd.read_csv("data/stored_urls.csv")
     labels = df['manual'].dropna().tolist()
@@ -119,8 +126,8 @@ if __name__ == "__main__":
             with open(f"data/matrices/{set_name}_{number_of_features}.pickle", 'rb') as f:
                 matrix = pickle.load(f)
             matrix_resampled, labels_resampled = smote.fit_resample(matrix, labels)
-            clf, result = create_model(matrix, labels, number_of_features, "Class weights")
-            clf_resampled, result_resampled = create_model(matrix_resampled, labels_resampled, number_of_features, "Oversampling")
+            clf, result = create_model(matrix, labels, set_name, number_of_features, "Class weights")
+            clf_resampled, result_resampled = create_model(matrix_resampled, labels_resampled, set_name, number_of_features, "Oversampling")
             # Save the models
             with open(f"data/models/cnn_{set_name}_{number_of_features}.pickle", 'wb') as f:
                 pickle.dump(clf, f)
